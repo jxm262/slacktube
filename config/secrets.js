6e8239d6122ccdeb61cccc642cb2859b42c0bbf2
Justin Maat <justin.maat@wendys.com>
2015-12-02 12:36:34 -0500

    Adding secrets config file for clientid, redirect url

---
+++ config/secrets.js
@@ -0,0 +1,22 @@
+/**
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ *
+ * You should never commit this file to a public repository on GitHub!
+ * All public code on GitHub can be searched, that means anyone can see your
+ * uploaded secrets.js file.
+ *
+ * I did it for your convenience using "throw away" API keys and passwords so
+ * that all features could work out of the box.
+ *
+ * Another added benefit of this approach is that you can use two different
+ * sets of keys for local development and production mode without making any
+ * changes to the code.
+
+ * IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT  IMPORTANT
+ */
+
+module.exports = {
+    CLIENT_ID: 'YOUR CLIENT ID HERE',
+    CLIENT_SECRET: 'YOUR CLIENT SECRET HERE',
+    REDIRECT_URL: 'YOUR REDIRECT URL HERE'
+};
