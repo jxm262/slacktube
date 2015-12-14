ac0768e627c2c6fbfa8cb7ef467c8824f2e480f4
Justin Maat <justin.maat@wendys.com>
2015-12-13 23:38:07 -0500

    fixing config file

--- config/index.js
+++ config/index.js
@@ -17,18 +17,18 @@
 
 module.exports = {
     mongo: {
-        username: 'justin',
-        password: 'Password1234',
-        url: 'localhost:27017',
-        database: 'test'
+        username: '',
+        password: '',
+        url: '',
+        database: ''
     },
     server: {
         hostname: 'localhost',
         port: 9004
     },
     google: {
-        CLIENT_ID: '829952237297-d8t8ub8k10vokjofmn27c25ka2a495a8.apps.googleusercontent.com',
-        CLIENT_SECRET: 'eWzVARvtLsM-hfmQGPP3i4NZ',
-        REDIRECT_URL: 'http://localhost:9004/youtube/oauthcallback'
+        CLIENT_ID: '',
+        CLIENT_SECRET: '',
+        REDIRECT_URL: ''
     }
 };
