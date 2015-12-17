8f300e1b2fff270beec7efbb44fafd735ab25d27
Justin Maat <justin.maat@wendys.com>
2015-12-17 15:49:27 -0500

    updating config

--- config/index.js
+++ config/index.js
@@ -17,21 +17,21 @@
 
 module.exports = {
     mongo: {
-        username: 'justin',
-        password: 'Password1234',
-        url: 'localhost:27017',
-        database: 'test'
+        username: '',
+        password: '',
+        url: '',
+        database: ''
     },
     jwt: {
-        key: 'eWzVARvtLsMhfmQGdDGSbb72PP3i4NZ'
+        key: ''
     },
     server: {
-        hostname: 'localhost',
-        port: 9004
+        hostname: '',
+        port: 8080
     },
     google: {
-        CLIENT_ID: '829952237297-d8t8ub8k10vokjofmn27c25ka2a495a8.apps.googleusercontent.com',
-        CLIENT_SECRET: 'eWzVARvtLsM-hfmQGPP3i4NZ',
-        REDIRECT_URL: 'http://localhost:9004/youtube/oauthcallback'
+        CLIENT_ID: '',
+        CLIENT_SECRET: '',
+        REDIRECT_URL: ''
     }
 };
